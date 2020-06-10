62f11fbd608ee88035a2d51f41752da7d0b2936b
Joshua Comeau <joshwcomeau@gmail.com>
2020-06-10 19:46:13 -0400

    Tweaks

--- server.js
+++ server.js
@@ -8,22 +8,27 @@ const { handleHomePage, handleFormData, handle404 } = require('./handlers');
 const PORT = process.env.PORT || 8000;
 
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('dev'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
-    .set('view engine', 'ejs')
+  .use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  })
+  .use(morgan('dev'))
+  .use(express.static('public'))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .set('view engine', 'ejs')
 
-    // endpoints
-    .get('/', handleHomePage)
-    .post('/form-data', handleFormData)
+  // endpoints
+  .get('/', handleHomePage)
+  .post('/form-data', handleFormData)
 
-    // handle 404s
-    .use(handle404)
+  // handle 404s
+  .use(handle404)
 
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .listen(PORT, () =>
+    console.log(`App ready! Access at http://localhost:${PORT}/`)
+  );
